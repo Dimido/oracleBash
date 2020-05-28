a3a2a593f09515e04fd29d8321b0e94bf6de8976
Dimido <34349799+Dimido@users.noreply.github.com>
2020-05-28 15:00:37 +0200

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+i=$2
+#echo $i
+file=$1
+#echo $file
+function oracle {
+
+#echo $i
+#echo $file
+
+eval "grep -e ${i} ${file}" > test.txt 2> err.txt
+if [ -s err.txt ]; then
+    echo "syntax error"
+    return 180
+else
+    if [ -s test.txt ]; then
+        echo "fichier non vide"
+        return 0
+    else
+        echo "fichier vide"
+        return 0
+    fi
+    
+fi
+}
+oracle
